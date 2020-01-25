a117ac0cbd9d9ee0af3c4bd2ae508959ec41d35d
Jatin <jatin.shashoo@gmail.com>
2020-01-26 03:02:30 +0530

    Hytrix Dashboard configured

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -13,15 +14,17 @@ import org.springframework.web.reactive.function.client.WebClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate(){
-		//To set timeout at bean level
-		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		//To set timeout at bean level: Dont use if using Hystrix
+		/*HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
 		factory.setConnectTimeout(3000);
-		return new RestTemplate(factory);
+		return new RestTemplate(factory);*/
+		return new RestTemplate();
 	}
 
 	@Bean

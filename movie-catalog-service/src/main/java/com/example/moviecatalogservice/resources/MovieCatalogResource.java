5607ca99d5fbf84dbe447417bba378400a2659da
Jatin <jatin.shashoo@gmail.com>
2020-01-26 00:32:37 +0530

    Init commit to enable Hystrix

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.example.moviecatalogservice.resources;
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
 import com.example.moviecatalogservice.models.UserRating;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,21 +26,27 @@ public class MovieCatalogResource {
     private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
         UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/"+userId,  UserRating.class);
-
         return userRating.getUserRating().stream().map(rating ->
                 {
                     Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
-                    /*Movie movie = webClientBuilder.build()
+                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+                }
+        ).collect(Collectors.toList());
+    }
+
+    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId){
+        return Arrays.asList(new CatalogItem("No Movie", "", 0));
+    }
+}
+
+/*
+Alternate WebClient way:
+Movie movie = webClientBuilder.build()
                             .get()
                             .uri("http://localhost:8082/movies/"+rating.getMovieId())
                             .retrieve()
                             .bodyToMono(Movie.class)
                             .block();*/
-                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
-                }
-        ).collect(Collectors.toList());
-
-    }
-}

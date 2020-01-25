f6d789a83f73c4f19c328d2c55f0045bd8496059
Jatin <jatin.shashoo@gmail.com>
2020-01-25 17:19:12 +0530

    Actual implementation of Service discovery using Eureka Server at MovieCatalog client

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -25,11 +25,11 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId,  UserRating.class);
+        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/"+userId,  UserRating.class);
 
         return userRating.getUserRating().stream().map(rating ->
                 {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
                     /*Movie movie = webClientBuilder.build()
                             .get()
                             .uri("http://localhost:8082/movies/"+rating.getMovieId())
